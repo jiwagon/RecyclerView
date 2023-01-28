6b4ce4172f359304e56a7a3da092d93fff88a766
jiwagon <jiwagon11@gmail.com>
2023-01-28 15:40:23 -0500

    adding data

--- app/src/main/java/com/example/recyclerview/StudentListActivity.java
+++ app/src/main/java/com/example/recyclerview/StudentListActivity.java
@@ -4,5 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class StudentListActivity extends AppCompatActivity {
 
-
+    private final StudentData[] STUDENT_LIST = {
+            new StudentData("John Doe", R.drawable.boy1, "jmd1234@psu.edu", "614-881-7600", "entrepreneurship, pitch competition"),
+            new StudentData("Jane Doe", R.drawable.girl1, "jnd4353@psu.edu", "614-789-3412", "machine learning, software development")
+    };
 }
