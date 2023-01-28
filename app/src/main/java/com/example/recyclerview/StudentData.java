096da3e4307928b78c16cf8536ef8e171f52e5d8
jiwagon <jiwagon11@gmail.com>
2023-01-28 14:59:19 -0500

    functional database

--- app/src/main/java/com/example/recyclerview/StudentData.java
+++ app/src/main/java/com/example/recyclerview/StudentData.java
@@ -1,4 +1,46 @@
 package com.example.recyclerview;
 
 public class StudentData {
+
+    final private String studentName;
+    final private int profileImage;
+    final private String email;
+    final private String phone;
+    final private String eventInterest;
+
+    public StudentData(String studentName, int profileImage,
+                      String email, String phone, String eventInterest) {
+        this.studentName = studentName;
+        this.email = email;
+        this.phone = phone;
+        this.profileImage = profileImage;
+        this.eventInterest = eventInterest;
+    }
+
+
+    public String getCampusName() {
+
+        return studentName;
+    }
+
+    public String getEmail() {
+
+        return email;
+    }
+
+    public String getPhone() {
+
+        return phone;
+    }
+
+    public String getEventInterest() {
+
+        return eventInterest;
+    }
+
+    public int getProfileImage() {
+
+        return profileImage;
+    }
+
 }
